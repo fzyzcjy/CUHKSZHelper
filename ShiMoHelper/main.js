5dd38347b59a86c9014b5e98e672d0604dff9ad1
turtlegood <turtlegood@163.com>
2017-11-12 20:22:44 +0800

    feat: Ctrl+F10/F11可以切换顶部两个栏的显示隐藏，Ctrl+F12可以调整界面，使之方便大字显示；自动隐藏左下角浮动

--- ShiMoHelper/main.js
+++ ShiMoHelper/main.js
@@ -22,23 +22,43 @@ function setTextCfg(isBold, isBlack) {
     setColor(isBlack ? 2 : 11);
 }
 
+function toggleDisplay(ele) {
+    $(ele).toggle();
+}
+
 function onKeyDown(e) {
-    if(e.ctrlKey) {
-        var succeed = true;
+    var succeed = true;
+    if(e.ctrlKey && !e.shiftKey) {
         switch(e.key) {
         case 'F1': setTextCfg(true, true); break;
         case 'F2': setTextCfg(false, true); break;
         case 'F3': setTextCfg(false, false); break;
+        case 'F10': toggleDisplay('#header-wrap'); break;
+        case 'F11': toggleDisplay('#edit-heading'); break;
+        case 'F12': $('body').toggleClass('css-modify'); break;
         default: succeed = false; break;
         }
-        if(succeed) {
-            console.log("Action", e.key);
+    } else if(!e.ctrlKey && e.shiftKey) {
+        switch(e.key) {
+        default: succeed = false; break;
         }
+    } else {
+        succeed = false;
+    }
+    if(succeed) console.log("Action", e.key);
+}
+
+function injectDOM() {
+    if($(".fixed-box").length == 0) {
+        setTimeout(injectDOM, 500);
+    } else {
+        $(".fixed-box").hide();
+        console.log("ShiMo Helper InjectedDOM");
     }
 }
 
 console.log("ShiMo Helper Loading");
 $(()=>{
+    injectDOM();
     $("body").keydown(onKeyDown);
-    console.log("ShiMo Helper Loaded");
 });
