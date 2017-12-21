5362f70bb787e8537a5b5606ac9b5d4d62e6240b
turtlegood <turtlegood@163.com>
2017-12-21 09:31:27 +0800

    feat: Go back to work with iframe(to make focus easier)

--- EnrolHelper/main.js
+++ EnrolHelper/main.js
@@ -26,6 +26,8 @@ $.fn.extend({
     }
 });
 
+var IFRAME_SELECTOR = "iframe#ptifrmtgtframe";
+
 var floatHtmlTemplate = '';
 
 var globalCnt = 0;
@@ -100,16 +102,20 @@ window.onmessage = function(e) {
     }
 };
 
-function registerHotKeyPassFromIframe() {
+function registerHotKeyPassToIframe() {
     var uniqueHotKeyArr =hotKeyDataArr.map(data => data.hotKey).filter((v, i, a) => a.indexOf(v) === i);
     for(var i = 0; i<uniqueHotKeyArr.length; ++i) {
         var hotKey = uniqueHotKeyArr[i];
-        Mousetrap.bind(hotKey, () => {
-            window.top.postMessage({
-                action: 'HotKey',
-                hotKey: hotKey,
-            }, '*');
-        });
+        if(!hotKey) continue;
+        Mousetrap.bind(hotKey, (function() {
+            var hk = hotKey;
+            return () => {
+                $(IFRAME_SELECTOR)[0].contentWindow.postMessage({
+                    action: 'HotKey',
+                    hotKey: hk,
+                }, '*');
+            };
+        })() );
     }
 }
 
@@ -133,25 +139,25 @@ hotKeyDataArr.push({
         location.href = 'http://116.31.95.2:81/psp/csprd/EMPLOYEE/HRMS/?&cmd=login&languageCd=ENG';
     },
 });
-hotKeyDataArr.push({
-    name: 'OptimizedMode',
-    text: 'Go into Optimized Mode',
-    hotKey: 'f4',
-    mode: 'global',
-    isActive() {
-        return $("iframe#ptifrmtgtframe").length > 0
-            || location.href.indexOf('STUDENT_HOMEPAGE') != -1;
-    },
-    onTrigger() {
-        var go = '';
-        if($("iframe#ptifrmtgtframe").length > 0) {
-            go = $("iframe#ptifrmtgtframe")[0].contentDocument.location.href;
-        } else {
-            go = 'http://116.31.95.2:81/psc/csprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ENROLLMENT.HC_SSR_SSENRL_CART_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder';
-        }
-        location.href = go;
-    }
-});
+// hotKeyDataArr.push({
+//     name: 'OptimizedMode',
+//     text: 'Go into Optimized Mode',
+//     hotKey: 'f4',
+//     mode: 'global',
+//     isActive() {
+//         return $("iframe#ptifrmtgtframe").length > 0
+//             || location.href.indexOf('STUDENT_HOMEPAGE') != -1;
+//     },
+//     onTrigger() {
+//         var go = '';
+//         if($("iframe#ptifrmtgtframe").length > 0) {
+//             go = $("iframe#ptifrmtgtframe")[0].contentDocument.location.href;
+//         } else {
+//             go = 'http://116.31.95.2:81/psc/csprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ENROLLMENT.HC_SSR_SSENRL_CART_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder';
+//         }
+//         location.href = go;
+//     }
+// });
 hotKeyDataArr.push(Object.assign({}, HIGHLIGHT_TEMPLATE, {
     name: 'Login',
     bindSelector: '.ps_signinentry input[type=submit]'
@@ -186,7 +192,6 @@ hotKeyDataArr.push({
                     $.map($("#SSR_CLSRCH_WRK_SUBJECT\\$0 option"), op => op.value)
                         .filter(x => x)
             };
-            console.log($("#SSR_CLSRCH_WRK_SUBJECT\\$0 option"), data);
             var $html = $(tmpl(template, data));
             $("body #win0divDERIVED_CLSRCH_GROUP2").parent().prepend($html);
             $html.on('click', '.subject', function() {
@@ -202,29 +207,32 @@ hotKeyDataArr.push({
 })
 //TODO $("a[id*=viewall]").advancedClick()
 
-function registerEvents() {
-    $("body").click(() => {
-        $("body").addClass('hide-focus-hint');
-    })
+function isMainFrameMode() {
+    return $(IFRAME_SELECTOR).length == 0 || inIframe();
+}
+
+function changeFocus() {
+    setTimeout(() => {
+        $("body").focus();
+    }, 0);
 }
 
 function boot() {
-
-    $.get(getURL('inject.template.html'), function(data) {
-        $("body").append(data);
-    });
-
     $.get(getURL('float.template.html'), function(data) {
         console.log("Boot");
         floatHtmlTemplate = data;
-        if(inIframe()) {
-            registerHotKeyPassFromIframe();
-        } else {
+        if(isMainFrameMode()) {
+            console.log("Main frame mode");
             createAllHotKey();
-            registerEvents();
+            changeFocus();
+            $.get(getURL('inject.template.html'), function(data) {
+                $("body").append(data);
+            });
+        } else {
+            console.log("Assistant frame mode");
+            registerHotKeyPassToIframe();
         }
     });
-
 }
 
 function reboot() {
@@ -235,12 +243,11 @@ function reboot() {
     // clear html
     $('.injected-dom').remove();
     $('.highlight').removeClass('highlight');
-    // reset mousetrap
-    if(inIframe()) {
-        //
-    } else {
+
+    if(isMainFrameMode()) {
         Mousetrap.reset();
         createAllHotKey();
+        changeFocus();
     }
 }
 
